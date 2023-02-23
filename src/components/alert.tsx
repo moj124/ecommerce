0bc7f79c9c17fee2cf59931aee2a74dc8ce6147b
Michael Owen Jones <michael.jowen.jones@gmail.com>
2023-02-23 22:46:02 +0000

    initial commit

---
+++ src/components/alert.tsx
@@ -0,0 +1,16 @@
+import styles from './alert.module.css';
+import cn from 'classnames';
+import { ReactNode }  from 'react';
+
+export default function Alert({ children, type } : { children: ReactNode, type: string}) {
+  return (
+    <div
+      className={cn({
+        [styles.success]: type === 'success',
+        [styles.error]: type === 'error',
+      })}
+    >
+      {children}
+    </div>
+  );
+}
